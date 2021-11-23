4db1837efe1e2025e09e68f979ffa820ba64aaa7
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-11-23 11:08:55 -0600

    implemented data fetching for climate chages

--- Global Watch/js/main.js
+++ Global Watch/js/main.js
@@ -1,5 +1,14 @@
+const API_URL = "http://localhost:3000";
+
+async function getCountryClimateChanges() {
+    const response = await fetch(`${API_URL}/1_climate-change.xlsx`);
+    const fileData = await response.arrayBuffer();
+    const workbook = XLSX.read(fileData, { type: "binary" });
+    return XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+}
+
 async function windowIsLoaded() {
-    console.log(GOOGLE_DRIVE_API_KEY);
+    await getCountryClimateChanges();
 }
 
 window.addEventListener("load", windowIsLoaded);
