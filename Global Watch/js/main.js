f0ade82ed34af068d6e42434fb566ab05e8ca80a
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-11-23 13:14:08 -0600

    implemented excel data fetching

--- Global Watch/js/main.js
+++ Global Watch/js/main.js
@@ -1,14 +1,36 @@
 const API_URL = "http://localhost:3000";
+const FILE_NAMES = {
+    averageMonthlyPrecipitations: "AverageMonthlyPrecipitations",
+    climateChanges: "ClimateChange",
+    coEmissions: "COEmissions",
+    futurePopulationProjections: "FuturePopulationProjections",
+    totalGHGEmissions: "TotalGHGEmissions",
+};
 
-async function getCountryClimateChanges() {
-    const response = await fetch(`${API_URL}/1_climate-change.xlsx`);
-    const fileData = await response.arrayBuffer();
-    const workbook = XLSX.read(fileData, { type: "binary" });
-    return XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+/**
+ * Reads the data from an excel file
+ * @param {string} datasetName The name of the data set to read
+ * @returns An array of objects
+ */
+async function fetchDataFromExcelFile(datasetName) {
+    try {
+        const response = await fetch(`${API_URL}/${datasetName}.xlsx`);
+        const fileData = await response.arrayBuffer();
+        // Read data as excel file
+        const workbook = XLSX.read(fileData, { type: "binary" });
+        return XLSX.utils.sheet_to_json(
+            workbook.Sheets[workbook.SheetNames[0]]
+        );
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function windowIsLoaded() {
-    await getCountryClimateChanges();
+    const climateChangeData = await fetchDataFromExcelFile(
+        FILE_NAMES.climateChanges
+    );
+    console.table(climateChangeData);
 }
 
 window.addEventListener("load", windowIsLoaded);
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-Visualizacion-PRY2-Global-Watch
+# Global Watch
+
+> Front-end web application for visualizing information related to climate change.
+
+## Course
+
+Information Visualization
+
+## Installation and setup
+
+### Serving the data files
+
+To run the server you must install an npm package on your system called [http-server](https://www.npmjs.com/package/http-server) with the following command:
+
+```
+$ npm install -g http-server
+```
+
+Once you have `http-server` installed you can run a local server in the `Server/` directory with the following command:
+
+```bash
+$ http-server -p 3000 --cors # Runs the server at http://localhost:3000
+```
+
+### Starting the website
+
+To start the website you can simply open the `index.html` file in your browser of preference.
+
+## Application Info
+
+### Version
+
+1.0.0
+
+### Authors
+
+Ignacio Álvarez & Andrés Aguilar.
