4885fa6e0c59c8fd404dcea740ae2c7dc9711fc9
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-11-26 22:16:46 -0600

    implemented temperature anomaly selector

--- Global Watch/js/main.js
+++ Global Watch/js/main.js
@@ -8,6 +8,19 @@ const FILE_NAMES = {
 };
 
 /**
+ * Loads the event listeners for the page
+ */
+function loadEventListeners() {
+    // Add event listener to temperature anomaly selectors
+    const temperatureAnomalySelectors = Array.from(
+        document.querySelectorAll(".temperature-anomaly-selector")
+    );
+    temperatureAnomalySelectors.map((selector) => {
+        selector.addEventListener("click", onTemperatureAnomalyClick);
+    });
+}
+
+/**
  * Reads the data from an excel file
  * @param {string} datasetName The name of the data set to read
  * @returns An array of objects
@@ -30,16 +43,43 @@ async function fetchDataFromExcelFile(datasetName) {
     }
 }
 
-async function windowIsLoaded() {
+async function onTemperatureAnomalyClick(event) {
+    event.preventDefault();
+
     const climateChangeData = await fetchDataFromExcelFile(
         FILE_NAMES.climateChanges
     );
+
+    const { selectorName } = event.target.dataset;
+    switch (selectorName) {
+        case "global":
+            console.log("its global");
+            break;
+        case "north":
+            console.log("its north");
+            break;
+        case "south":
+            console.log("its south");
+            break;
+        default:
+            return;
+    }
+}
+
+async function windowIsLoaded() {
+    loadEventListeners();
+    // Get initial data for temperature anomaly line chart
+    const climateChangeData = await fetchDataFromExcelFile(
+        FILE_NAMES.climateChanges
+    );
+    console.log(climateChangeData);
     const globalClimateChanges = climateChangeData.filter(
         (climateChange) => climateChange["Code"] === "OWID_WRL"
     );
     const climateChangesPerYear =
         getClimateChangesPeryYear(globalClimateChanges);
-    let tempAnomalyChart = createTemperatureAnomaliesChart(
+    // Create line chart
+    createTemperatureAnomaliesChart(
         {
             title: "Anomalías de temperatura globales por año",
             yAxisTitle: "Temperatura",
